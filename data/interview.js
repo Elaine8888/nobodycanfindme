5ac2f76493afd959c8c1fcda8e6fbc6723939f93
Elaine8888 <35128324+Elaine8888@users.noreply.github.com>
2026-03-30 19:28:54 +0800

    Create interview.js

---
+++ data/interview.js
@@ -0,0 +1,22 @@
+const interviewData = [
+  {
+    id: 1,
+    topic: "Friendship",
+    questions: [
+      "Do you have a large group of friends, or do you prefer to have a smaller group of friends?",
+      "Describe a time when a friendship taught you something valuable, or helped you see something from a new perspective.",
+      "In your opinion, what makes a friendship last a long time?",
+      "Some people believe that maintaining friendships is essential for emotional well-being and life satisfaction. Do you agree or disagree? Why?"
+    ]
+  },
+  {
+    id: 2,
+    topic: "Movies",
+    questions: [
+      "What kind of movies do your family or friends generally like to watch? For example, do they prefer action movies, comedies, dramas, or other types?",
+      "When you watch a movie, do you prefer to watch after work or school on weekdays, or do you like to watch during the weekends? Why?",
+      "In the past, people mostly watched movies in theaters. Today, many people watch movies at home. Do you think that movie theaters will continue to exist in the future? Why or why not?",
+      "Some people believe that movies can be a powerful tool for educating people and raising awareness about important issues. Do you agree with this idea, or do you think there are other, more effective ways to educate people?"
+    ]
+  }
+];
