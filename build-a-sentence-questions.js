dcdd69b0eb57317afdd00622752d6f1f955cfbc2
Elaine8888 <35128324+Elaine8888@users.noreply.github.com>
2026-04-02 21:25:43 +0800

    Create build-a-sentence-questions.js

---
+++ build-a-sentence-questions.js
@@ -0,0 +1,18 @@
+const buildSentenceQuestions = [
+  {
+    id: 1,
+    speakerA: "The assignment deadline has been extended.",
+    speakerBPrefix: "Do",
+    speakerBSuffix: "?",
+    bank: ["the new", "due date", "know", "has been announced", "you", "if"],
+    answer: ["you", "know", "if", "the new", "due date", "has been announced"]
+  },
+  {
+    id: 2,
+    speakerA: "Did you get a chance to review the report?",
+    speakerBPrefix: "Sorry, but I",
+    speakerBSuffix: "yet.",
+    bank: ["to look", "not", "at", "time", "have", "had", "it"],
+    answer: ["have", "not", "had", "time", "to look", "at", "it"]
+  }
+];
